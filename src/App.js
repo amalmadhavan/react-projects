241d4ee838f0e7a00dc994c23c8a6731acb2df46
Amal Madhavan <amalmadhavan11@gmail.com>
2021-09-20 10:10:38 +0530

    first project done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+    <h1> React File Tree App </h1>
+    <Folder name="Desktop" >
+      <File name="cats.png" />
+      <File name="dogs.pdf" />
+      <Folder name="music">
+        <File name="king.mp3" />
+        <File name="attention.mp3" />
+      </Folder>
+    </Folder>
+    <Folder name="Applications" >
+      <File name="chrome" />
+      <File name="fortnite" />
+    </Folder>
+  </div>
+
 }
+
+// React Component name should always start with a Capital letter
+
+const Folder = (props) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const { name, children } = props
+  const arrowDir = isOpen ? "down" : "right"
+
+  const handleCLick = () => {
+    setIsOpen(!isOpen)
+  }
+  return <div>
+    <span onClick={handleCLick}>
+      <i className="blue folder icon"></i>
+      <i class={`caret ${arrowDir} icon`}></i>
+      {name}</span>
+    <div style={{ marginLeft: '20px' }}>
+      {isOpen ? children : null}
+    </div>
+
+  </div >
+};
+
+const File = (props) => {
+  return <div> {props.name}</div>
+};
 
 export default App;
